f98062a6db1afed5784822c61097937b3e744771
umar006 <umaralfaruq597@gmail.com>
2023-10-24 22:00:57 +0700

    Call api to fetch searched comic list data from asura sites

--- extensions/popup.js
+++ extensions/popup.js
@@ -15,6 +15,27 @@ function dumpTreeNodes(bookmarkNodes) {
   }
 }
 
+async function fetchComics(title) {
+  // check curly single quote and get the index position
+  const check = title.indexOf("\u2019");
+
+  // check idx position > title.length / 2, so title don't to short
+  // e.g. I'm the best -> I
+  if (check !== -1 && check > Math.floor(title.length / 2)) {
+    title = title.slice(0, check);
+  }
+
+  const url = "http://localhost:3000/asura?s=" + encodeURIComponent(title);
+
+  try {
+    const raw = await fetch(url);
+    const json = await raw.json();
+    return json.data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   dumpBookmarks();
 });
