bda7266c4b2f5fc4e61e289a0c53bb4be6eada3a
umar006 <umaralfaruq597@gmail.com>
2023-10-24 21:45:34 +0700

    Filter bookmark to get comics from asura website

--- extensions/popup.js
+++ extensions/popup.js
@@ -1,27 +1,20 @@
-// Traverse the bookmark tree, and print the folder and nodes.
+// Traverse the bookmark tree
 function dumpBookmarks() {
   chrome.bookmarks.getSubTree("14", function (bookmarkTreeNodes) {
     if (bookmarkTreeNodes[0].title !== "manga") return;
 
-    console.log(bookmarkTreeNodes[0].children);
+    const mangaChildren = bookmarkTreeNodes[0].children;
+    dumpTreeNodes(mangaChildren);
   });
+}
+
+function dumpTreeNodes(bookmarkNodes) {
+  for (let i = 0; i < bookmarkNodes.length; i++) {
+    const isAsura = bookmarkNodes[i].url.includes("asuratoon");
+    if (!isAsura) continue;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   dumpBookmarks();
 });
-
-// // Search the bookmarks when entering the search keyword.
-// $("#search").change(function () {
-//   $("#bookmarks").empty();
-//   dumpBookmarks($("#search").val());
-// });
-
-// function dumpTreeNodes(bookmarkNodes, query) {
-//   const list = $("<ul>");
-//   for (let i = 0; i < bookmarkNodes.length; i++) {
-//     list.append(dumpNode(bookmarkNodes[i], query));
-//   }
-
-//   return list;
-// }
