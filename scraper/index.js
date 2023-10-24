f9035e87db731adb640dfaeb5409100bf736bf33
umar006 <umaralfaruq597@gmail.com>
2023-10-24 22:04:37 +0700

    Add cors so the extensions can call the api

--- scraper/index.js
+++ scraper/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { launch } from "puppeteer";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/asura", async (req, res) => {
   const { s } = req.query;
