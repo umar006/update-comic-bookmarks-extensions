a6a4bd2aae93f7d8f919a57e9e98f1f623a3de1a
umar006 <umaralfaruq597@gmail.com>
2023-11-18 05:55:49 +0700

    Add site to scrape from luminousscans

--- scraper/index.js
+++ scraper/index.js
@@ -14,6 +14,13 @@ app.get("/asura", async (req, res) => {
   res.json({ data: mangas });
 });
 
+app.get("/luminous", async (req, res) => {
+  const { s } = req.query;
+  const mangas = await mangasFromLuminous(s);
+
+  res.json({ data: mangas });
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
@@ -27,6 +34,34 @@ async function mangasFromAsura(query) {
   const page = await browser.newPage();
 
   await page.goto(`https://asuratoon.com/?s=${query}`, {
+    waitUntil: "domcontentloaded",
+  });
+
+  const mangas = await page.evaluate(() => {
+    const mangaList = document.querySelectorAll(".listupd > .bs");
+
+    return Array.from(mangaList).map((manga) => {
+      const title = manga.querySelector("a").title;
+      const url = manga.querySelector("a").href;
+
+      return { title, url };
+    });
+  });
+
+  await browser.close();
+
+  return mangas;
+}
+
+async function mangasFromLuminous(query) {
+  const browser = await launch({
+    headless: "new",
+    defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(`https://luminousscans.gg/?s=${query}`, {
     waitUntil: "domcontentloaded",
   });
 
